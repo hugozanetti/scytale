7285705c4925ee14e78c923aaeeeca9de1a2b4c2
hugozanetti <hugo.zanetti@gmail.com>
2019-02-10 23:01:42 -0200

    Inserção das textAreas com seus respectivos labels.

--- view/mainWindow.py
+++ view/mainWindow.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter.scrolledtext import *
 
 class MainWindow(Tk):
      def __init__(self):
@@ -8,6 +9,18 @@ class MainWindow(Tk):
         
      def initUI(self):
         frameGenKeys = Frame(self,  relief=RAISED,  borderwidth=1)
+        lbl = Label(frameGenKeys, text='Private Key:')
+        lbl.grid(column=0, row=0)
+        
+        txtAreaPrivateK = ScrolledText(frameGenKeys, width=40, height=10)
+        txtAreaPrivateK.grid(column=0, row=1)
+        
+        lbl = Label(frameGenKeys, text='Public Key:')
+        lbl.grid(column=1, row=0)
+        
+        txtAreaPublicK = ScrolledText(frameGenKeys, width=40, height=10)
+        txtAreaPublicK.grid(column=1, row=1)
+        
         btnKeyGen = Button(frameGenKeys, text='Generate Keys')
-        btnKeyGen.grid(row=0, column=0)
+        btnKeyGen.grid(column=0, row=2)
         frameGenKeys.pack(fill=X)
