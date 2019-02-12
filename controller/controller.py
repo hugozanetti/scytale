4c6b39e6f135675f38f8e3b59854ea953a5d8256
hugozanetti <hugo.zanetti@gmail.com>
2019-02-12 12:35:03 -0200

    Acertei a importação do package e os imports necessários para criptografia e questões do tkinter

--- controller/controller.py
+++
@@ -1,7 +0,0 @@
-from tkinter.scrolledtext import *
-from model.key import Key
-
-class CtrlKey:
-    
-    def generate(self, txtPrivKey:ScrolledText, txtPubKey:ScrolledText):
-        Key.privKey(txtPrivKey, txtPubKey)
