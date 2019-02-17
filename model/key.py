53cd9b032c490d1072a6eb14b8c3cacc881530e5
hugozanetti <hugo.zanetti@gmail.com>
2019-02-17 19:23:22 -0300

    Foi criado o método encrypting que substitui os antigos módulos criptMessage e deCriptMessage. Procurei utilizar condicionais para evitar escrever mesmo bloco de código mais de uma vez.

--- model/key.py
+++ model/key.py
@@ -1,11 +1,14 @@
 from tkinter import *
 from tkinter.scrolledtext import *
+from tkinter.ttk import Combobox
 import Crypto
 import Crypto.Random
 from Crypto.Hash import SHA
 from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_v1_5
 from Crypto.Cipher import PKCS1_OAEP
+import binascii
+import ast
 
 class Key:
 
@@ -14,8 +17,8 @@ class Key:
         private_key = RSA.generate(1024, random_gen)
         public_key = private_key.publickey()
         
-        secretKey = private_key.exportKey(format='DER')
-        visibleKey = public_key.exportKey(format='DER')
+        visibleKey = binascii.hexlify(public_key.exportKey(format='DER')).decode('ascii')
+        secretKey = binascii.hexlify(private_key.exportKey(format='DER')).decode('ascii')
         
 
         # Grava todas as chaves geradas pelo algorito RSA em um arquivo de texto
@@ -35,3 +38,42 @@ class Key:
         txtPrivKey.insert(INSERT, secretKey)
         txtPubKey.insert(INSERT, visibleKey)
         
+        
+     
+    def encrypting(self, txtPrivKey:ScrolledText, txtPubKey:ScrolledText, txtMsg:ScrolledText, txtEncriptedMsg:ScrolledText,  comboAction:Combobox):
+         strAction = comboAction.get()
+         chavePreenchida = False
+         if strAction == 'Encrypt':  
+             if txtPubKey.get('1.0', 'end-1c') != '':
+                 chavePreenchida = True
+                 cryptKey = PKCS1_OAEP.new(RSA.importKey(binascii.unhexlify(txtPubKey.get('1.0', 'end-1c'))))             
+         else: # Decrypt
+             if txtPrivKey.get('1.0', 'end-1c') != '':
+                 chavePreenchida = True
+                 cryptKey = PKCS1_OAEP.new(RSA.importKey(binascii.unhexlify(txtPrivKey.get('1.0', 'end-1c'))))
+         
+         
+         if not chavePreenchida:
+             f = open("C:/Python/scytale/data/keysfile.txt", "r")
+             file_lines = f.readlines()
+             f.close()
+             
+             if strAction == 'Encrypt':
+                 subtracao = 1 #public key
+             else:
+                 subtracao = 2 #private key
+                 
+                 
+             last_line = file_lines[len(file_lines)-subtracao] #last_line é chave que será usada na operação de criptografia!
+             cryptKey = PKCS1_OAEP.new(RSA.importKey(binascii.unhexlify(last_line.strip())))
+             
+             
+         if strAction == 'Encrypt':
+             b = bytes(str(txtMsg.get('1.0', 'end-1c')), "utf-8")
+             encrypted = cryptKey.encrypt(b)
+             #conversão (str) para jogar essa informação na caixa de texto da GUI !
+             txtEncriptedMsg.insert(INSERT, binascii.hexlify(encrypted).decode('ascii')) 
+         else:
+             decrypted = cryptKey.decrypt(binascii.unhexlify(txtEncriptedMsg.get('1.0', 'end-1c')))
+             txtMsg.insert(INSERT, str(decrypted.decode()))
+                 
