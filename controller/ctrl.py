0f8a1b4d87bcdf210e07aafd1aed2f0afb22c698
hugozanetti <hugo.zanetti@gmail.com>
2019-02-17 19:19:24 -0300

    Como fundi os métodos cript e deCript numa só coisa, onde o usuário escolhe qual ação deseja através do combobox, apenas criei o método encryptRun.

--- controller/ctrl.py
+++ controller/ctrl.py
@@ -1,7 +1,20 @@
 from tkinter.scrolledtext import *
+from tkinter.ttk import Combobox
 from model.key import Key
 
 class CtrlKey:
     
     def generate(self, txtPrivKey:ScrolledText, txtPubKey:ScrolledText):
         Key.privKey(self,  txtPrivKey, txtPubKey)
+        
+    def encryptRun(self, txtPrivKey:ScrolledText, txtPubKey:ScrolledText, txtMsg:ScrolledText, txtEncriptedMsg:ScrolledText,  comboAction:Combobox):
+         Key.encrypting(self, txtPrivKey, txtPubKey,  txtMsg,  txtEncriptedMsg,  comboAction)
+        
+    """"
+    def cript(self, txtPubKey:ScrolledText,  txtMsg:ScrolledText,  txtMsgEncoded:ScrolledText):
+        Key.criptMessage(self, txtPubKey, txtMsg,  txtMsgEncoded)
+        
+        
+    def deCript(self, txtPrivKey:ScrolledText,  txtMsg:ScrolledText,  txtMsgEncoded:ScrolledText):
+        Key.deCriptMessage(self, txtPrivKey, txtMsg,  txtMsgEncoded)
+    """
